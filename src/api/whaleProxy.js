b6506fd09f161b4363404095800bf3eaec0e87b3
Talbo1amdi <talboamdi@gmail.com>
2024-10-27 05:25:49 -0700

    Add files via upload

--- src/api/whaleProxy.js
+++ src/api/whaleProxy.js
@@ -1,5 +1,3 @@
-// src/api/whaleProxy.js
-
 import axios from 'axios';
 
 export default async function handler(req, res) {
@@ -7,12 +5,14 @@ export default async function handler(req, res) {
     const response = await axios.get('https://api.whale-alert.io/v1/transactions', {
       params: {
         api_key: process.env.REACT_APP_WHALE_API_KEY,
-        min_value: 10 * 100000000, // 10 BTC in satoshis
+        min_value: 10 * 100000000,
         currency: 'btc'
       }
     });
+    console.log('API Response:', response.data); // Log the API response
     res.status(200).json(response.data);
   } catch (error) {
+    console.error('Error fetching data from Whale API:', error.response ? error.response.data : error.message);
     res.status(500).json({ error: 'Error fetching data' });
   }
 }
